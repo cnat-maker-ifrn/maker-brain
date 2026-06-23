d6d44f26029ac4c11d9e80e72cb3d6b1a9624916
Lucas Tales <lucastales100@gmail.com>
2026-06-23 11:28:01 -0300

    fix: correct rabbitmq configs

--- maker-app/makerapp/makerapp/settings.py
+++ maker-app/makerapp/makerapp/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'makerapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
