e2aecbd5dfb19ffff395c6ecab0d04ccacb5c6be
unknown <lucastales100@gmail.com>
2026-07-14 10:40:14 -0300

    feat: add scholarship student register

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { RegisterPage } from '@/pages/RegisterPage';
+import { RegisterRequesterPage } from '@/pages/RegisterRequesterPage';
+import { RegisterScholarshipStudentPage } from '@/pages/RegisterScholarshipStudentPage';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/register" element={<RegisterRequesterPage />} />
+      <Route path="/register/bolsista" element={<RegisterScholarshipStudentPage />} />
     </Routes>
   );
 }
