2a3fff8bfa6d640344d3e74058bc459eb1f5048c
Lucas Tales <lucastales100@gmail.com>
2026-08-18 23:34:01 -0300

    feat: add login shortcut

--- frontend/src/features/makerauth/index.js
+++ frontend/src/features/makerauth/index.js
@@ -1,5 +1,9 @@
+export { authService } from './services/authService';
+
 export { RequesterRegisterForm } from './components/RequesterRegisterForm';
 export { ScholarshipStudentRegisterForm } from './components/ScholarshipStudentRegisterForm';
+export { LoginForm } from './components/LoginForm';
+
 export { useRegisterRequester } from './hooks/useRegisterRequester';
 export { useRegisterScholarshipStudent } from './hooks/useRegisterScholarshipStudent';
-export { authService } from './services/authService';
+export { useLogin } from './hooks/useLogin';
